5f35b0731e6f332a53e3394e7041cb52468ea1a6
BNI Russell <fougere.russell@brunswicknews.com>
2021-03-23 15:37:31 -0300

    Test 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi there</h1>
+    <h1>Hi Folks</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
